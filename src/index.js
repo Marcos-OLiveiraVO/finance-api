96d4bd44e6573a0e0212eb81eecaf3e5febcde86
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-12-22 00:33:18 -0300

    feat: create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { v4: uuidv4 } = require("uuid");
 
 app.listen(PORT, () => console.log("Server is running..."));
+app.use(express.json());
+
+const costumers = [];
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+  const id = uuidv4();
+
+  costumers.push({
+    name,
+    cpf,
+    id,
+  });
+
+  return res.send(costumers);
+});
