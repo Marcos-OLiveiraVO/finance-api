e0aae3cd85eafaffd501f6362c5faa809fee980d
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-12-22 14:13:06 -0300

    fix: cpf validator middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ app.use(express.json());
 
 const costumers = [];
 
+function verifyIfAccountCpfExist(req, res, next) {
+  const { cpf } = req.headers;
+
+  const costumer = costumers.find((costumer) => costumer.cpf === cpf);
+
+  if (!costumer) {
+    return res.status(400).send({ Error: "Costumer not found." });
+  }
+
+  req.costumer = costumer;
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
 
@@ -29,14 +43,7 @@ app.post("/account", (req, res) => {
   return res.status(201).send(costumers);
 });
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
-
-  const costumer = costumers.find((costumer) => costumer.cpf === cpf);
-
-  if (!costumer) {
-    return res.status(400).send({ Error: "User not found." });
-  }
-
+app.get("/statement", verifyIfAccountCpfExist, (req, res) => {
+  const { costumer } = req;
   return res.send(costumer.statement);
 });
